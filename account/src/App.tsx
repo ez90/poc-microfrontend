106b235a870e05bb08028accfec38ae9f3a42e7e
Mickael Mesnage <mickaelmesnage@gmail.com>
2021-03-19 17:44:45 +0100

    wip 3

--- account/src/App.tsx
+++ account/src/App.tsx
@@ -1,58 +1,30 @@
-import React, {useState} from 'react';
-import {Provider, useSelector, useDispatch} from 'react-redux';
+import React from 'react';
+import { Provider } from 'react-redux';
+
 import createStore from './mainRedux/createStore';
-import { setUserName } from './mainRedux/actions';
 import { modalReducer } from './redux/reducers/modal';
-import { setModalIsOpen } from './redux/actions';
 
-const App = () => {
-    const dispatch = useDispatch();
-    const user = useSelector((state) => state?.user);
-    const modal = useSelector((state) => state?.modal);
-    const [name, setName] = useState(user?.name);
-    console.log('eeeee', setModalIsOpen(true));
+import Settings from './components/Settings';
+import ShowModalButton from './components/ShowModalButton';
 
-    return (
-        <div style={{marginTop: '10px'}}>
-            <h1>App 2</h1>
-            <div>
-                Change the name
-            </div>
-
-            <div>
-                <input
-                    style={{marginRight: '10px'}}
-                    type="text"
-                    onChange={(e) => {
-                        setName(e.target.value);
-                    }}
-                />
-                <button onClick={() => dispatch(setUserName(name))}>
-                    Dispatch App 2 new name
-                </button>
-            </div>
-
-            <span>modal: {modal?.isOpen ? 'open' : 'close'}</span>
-            <button type="button" onClick={() => dispatch(setModalIsOpen(true))}>
-            {/* <button type="button" onClick={() => console.log('ddddd')}> */}
-                SetModal to true
-            </button>
-        </div>
-    );
-};
+const App = () => (
+  <>
+    <h1>App 2</h1>
+    <Settings />
+    <ShowModalButton />
+  </>
+);
 
 const AppWrapper = ({ store }) => {
+  const computedStore = store || createStore({});
 
-    const computedStore = store || createStore({});
+  computedStore.injectReducer('modal', modalReducer);
 
-    computedStore.injectReducer('modal', modalReducer);
-
-    return (
-        <Provider store={computedStore}>
-            <App/>
-        </Provider>
-    );
-
+  return (
+    <Provider store={computedStore}>
+      <App />
+    </Provider>
+  );
 };
 
 export default AppWrapper;
