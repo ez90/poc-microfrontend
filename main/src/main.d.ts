1124441e60046747419ea1fa618f58c34a9416c1
SetiZ <funkysetiz@gmail.com>
2021-03-19 09:56:56 +0100

    clean tsconfig + add types

---
+++ main/src/main.d.ts
@@ -0,0 +1,13 @@
+/// <reference types="react" />
+
+declare module "product/App" {
+  const Product: React.ComponentType;
+
+  export default Product;
+}
+
+declare module "account/App" {
+    const Account: React.ComponentType;
+  
+    export default Account;
+  }
