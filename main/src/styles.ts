da787ace243a049d4aa62109912a4b0e0080165c
SetiZ <funkysetiz@gmail.com>
2021-03-24 08:28:59 +0100

    add a bit of styling

--- main/src/styles.ts
+++ main/src/styles.ts
@@ -1,5 +1,18 @@
 import styled from 'styled-components';
+import { createGlobalStyle } from 'styled-components'
+
 
 export const SWrapper = styled.main`
+  margin: 0;
   padding: 25px;
+  background-color: #efefef;
+  font-family: 'Open Sans',Helvetica,Arial,Lucida,sans-serif;
 `;
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
+`
--- product/src/styles.ts
+++ product/src/styles.ts
@@ -2,5 +2,15 @@ import styled from 'styled-components';
 import { Link } from "react-router-dom";
 
 export const SLink = styled(Link)`
-  padding-right: 25px;
+  text-decoration: none;
+  padding-right: 10px;
+  height: 50px;
+  line-height: 50px;
+  &:visited {
+    color: #000;
+  }
+  &:hover {
+    /* background-color: #e2e2e2; */
+    text-decoration: underline;
+  }
 `;
