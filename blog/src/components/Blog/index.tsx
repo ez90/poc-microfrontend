401c7938e696a5ded7f63fad175631d6fefc6ff4
Julien Gonzalez <julien.gonzalez@thetribe.io>
2021-03-25 15:09:15 +0100

    Adds missing file

--- blog/src/components/Blog/index.tsx
+++ blog/src/components/Blog/index.tsx
@@ -1,20 +1,21 @@
 import React, { FC } from 'react';
-import { SPostWrapper, SPost, SPostTitle, SPostContent } from './styles';
+
+import {SPost, SPostTitle, SPostContent} from './styles';
 import Post from '../../types/post';
 
 export interface BlogProps {
     posts: Post[];
 }
 
-const Blog: FC<BlogProps> = ({posts}) => (
-    <SPostWrapper>
+const Blog: FC<BlogProps> = ({ posts }) => (
+    <>
         {posts.map((post) => (
             <SPost key={post.title}>
                 <SPostTitle>{post.title}</SPostTitle>
                 <SPostContent>{post.content}</SPostContent>
             </SPost>
         ))}
-    </SPostWrapper>
+    </>
 )
 
 export default Blog;
---
+++ blog/src/styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const SWrapper = styled.main`
+  padding: 0 25px;
+  background-color: #fff;
+`;
+
+export const SContent = styled.div`
+  padding: 25px 15px;
+  background-color: #fff;
+`;
